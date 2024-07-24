02859c4c2985e91b986916dc9f5bacdedc203755
HWANG-EunHyck <easton123@naver.com>
2024-07-24 09:05:40 +0900

    8

---
+++ DAY_07/ex_matplotlib.py
@@ -0,0 +1,10 @@
+# --------------------------------------------
+# Matplotlib 활용한 시각화
+# --------------------------------------------
+import matplotlib.pyplot as plt 
+
+# 그래프 그리기
+plt.plot([1,2,3,4])
+plt.xlabel('DATA')
+plt.ylabel('RESULT')
+plt.show()
